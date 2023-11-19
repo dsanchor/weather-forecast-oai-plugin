0b6f7f6c8eece58a5c08f07ef43a1c59f708eccf
dsanchor <sancheski@gmail.com>
2023-11-19 11:06:10 +0100

    Added SK to plugin

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
 # Open AI plugin for Weather Forecast
 
-# Github
+In this example, we will create a plugin that uses the Open Weather API to get the weather forecast for a given city.
+
+You may then deploy using the following methods:
+- [Deploy to AKS from Github Actions](#1-deploy-to-aks-from-github-actions)
+- [Manual deploy image in App Service](#2-manual-deploy-image-in-app-service)
+
+# 1. Deploy to AKS from Github Actions
 
 ## Create secrets
 
+```bash
 AKS_RESOURCE_GROUP=<resource group>
 AKS_CLUSTER_NAME=<cluster name>
 AKS_NAMESPACE=chat-copilot-plugins
 AZURE_CREDENTIALS=<sp-json>
+CLIENT_AZUREOPENAI_KEY=<client-azureopenai-key>
+CLIENT_AZUREOPENAI_ENDPOINT=<client-azureopenai-endpoint>
+CLIENT_AZUREOPENAI_DEPLOYMENTNAME=<client-azureopenai-deploymentname>
+```
 
-# Prepare AKS environment
+# Prepare AKS environment for github actions
 
 ## Environment variables
 
@@ -29,4 +40,35 @@ az aks get-credentials --resource-group $RESOURCE_GROUP --name $CLUSTER_NAME
 
 ```bash
 kubectl create namespace $NAMESPACE
+```
+
+# 2.  Manual deploy image in App Service
+
+## Create App Service
+
+```bash
+RESOURCE_GROUP=<app-service-rg>
+TAG=$(git rev-parse HEAD)
+IMAGE=ghcr.io/dsanchor/weather-forecast-oai-plugin:$TAG
+az appservice plan create --name chat-copilot-plugins --resource-group $RESOURCE_GROUP --sku S1 --is-linux
+az webapp create --resource-group $RESOURCE_GROUP --plan chat-copilot-plugins --name chat-copilot-plugins --deployment-container-image-name $IMAGE
+```
+
+## Set environment variables
+
+```bash
+DOMAIN=$(az webapp show --name chat-copilot-plugins --resource-group $RESOURCE_GROUP --query defaultHostName --output tsv)
+CLIENT_AZUREOPENAI_KEY=<client-azureopenai-key>
+CLIENT_AZUREOPENAI_ENDPOINT=<client-azureopenai-endpoint>
+CLIENT_AZUREOPENAI_DEPLOYMENTNAME=<client-azureopenai-deploymentname>
+
+az webapp config appsettings set --resource-group $RESOURCE_GROUP --name chat-copilot-plugins --settings OAI_PLUGIN_BASEURL=https://$DOMAIN CLIENT_AZUREOPENAI_KEY=$CLIENT_AZUREOPENAI_KEY CLIENT_AZUREOPENAI_ENDPOINT=$CLIENT_AZUREOPENAI_ENDPOINT CLIENT_AZUREOPENAI_DEPLOYMENTNAME=$CLIENT_AZUREOPENAI_DEPLOYMENTNAME
+```
+
+## Redeploy image with latest commit
+
+```bash
+TAG=$(git rev-parse HEAD)
+IMAGE=ghcr.io/dsanchor/weather-forecast-oai-plugin:$TAG
+az webapp config container set --resource-group $RESOURCE_GROUP --name chat-copilot-plugins --docker-custom-image-name $IMAGE
 ```
---
+++ src/main/java/com/microsoft/stu/weatherforecastoaiplugin/service/SemanticKernel.java
@@ -0,0 +1,135 @@
+package com.microsoft.stu.weatherforecastoaiplugin.service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.core.credential.AzureKeyCredential;
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.SKBuilders;
+import com.microsoft.semantickernel.connectors.ai.openai.util.AzureOpenAISettings;
+import com.microsoft.semantickernel.exceptions.ConfigurationException;
+import com.microsoft.semantickernel.orchestration.ContextVariables;
+import com.microsoft.semantickernel.orchestration.SKContext;
+import com.microsoft.semantickernel.planner.actionplanner.Plan;
+import com.microsoft.semantickernel.planner.sequentialplanner.SequentialPlanner;
+import com.microsoft.semantickernel.skilldefinition.ReadOnlyFunctionCollection;
+import com.microsoft.semantickernel.skilldefinition.ReadOnlySkillCollection;
+import com.microsoft.stu.weatherforecastoaiplugin.plugins.Forecast;
+
+import jakarta.annotation.PostConstruct;
+import reactor.core.publisher.Mono;
+
+@Service
+public class SemanticKernel {
+
+    @Value("${client.azureopenai.key}")
+    private String azOaiKey;
+
+    @Value("${client.azureopenai.endpoint}")
+    private String azOaiEndpoint;
+
+    @Value("${client.azureopenai.deploymentname}")
+    private String azOaiDeployment;
+
+
+    private Kernel kernel;
+
+    @PostConstruct
+    public void init() {
+        System.out.println("SemanticKernel initialization");
+        try {
+            kernel = kernel();
+        } catch (ConfigurationException e) {
+            System.out.println("SemanticKernel initialization failed");
+        }
+    }
+
+    public String getWeatherForecastSummary(String location) {
+
+        System.out.println("=> Run the Kernel");
+        
+        ContextVariables variables = SKBuilders.variables()
+                .withInput(location)
+                .build();
+                
+        Mono<SKContext> result =
+        getKernel().runAsync(
+                variables,
+                getKernel().getSkill("Locator").getFunction("CityCoordinates", null),
+                getKernel().getSkill("Forecast").getFunction("GetForecastJsonData", null),
+                getKernel().getSkill("Weather").getFunction("ForecastSummarizer", null));
+
+        System.out.println("=> Result");
+        String summary = result.block().getResult();
+        System.out.println(summary);
+
+        return summary;
+    }
+
+    /**
+    public String plan(String goal) {
+        // create a planner
+        SequentialPlanner planner = new SequentialPlanner(getKernel(), null, null);
+        // create a plan
+        Plan plan = planner.createPlanAsync(goal).block();
+        System.out.println("=> Plan <=");
+        System.out.println(plan.toPlanString());
+        // execute the plan
+        String result = plan.invokeAsync().block().getResult();
+        System.out.println("=> Result <=");
+        System.out.println(result);
+        return result;
+    }
+    */
+
+    private Kernel kernel() throws ConfigurationException {
+
+        OpenAIAsyncClient client = openAIAsyncClient();
+
+        Kernel kernel = SKBuilders.kernel()
+                .withDefaultAIService(SKBuilders.chatCompletion()
+                        .withModelId(settings().getDeploymentName())
+                        .withOpenAIClient(client)
+                        .build())
+                .build();
+
+        kernel.importSkillFromDirectory("Locator", "src/main/resources/plugins", "Locator");
+        kernel.importSkillFromDirectory("Weather", "src/main/resources/plugins", "Weather");
+        kernel.importSkill(new Forecast(), "Forecast");
+
+        ReadOnlySkillCollection collection =  kernel.getSkills();
+        ReadOnlyFunctionCollection functions = collection.getAllFunctions();
+        System.out.println("=> Registered functions <=");
+        functions.getAll().forEach(function -> {
+            System.out.println(function.getSkillName()+"."+function.getName());
+        });
+        return kernel;
+    }
+
+    private OpenAIAsyncClient openAIAsyncClient() throws ConfigurationException {
+        // Create an Azure OpenAI client
+        AzureOpenAISettings settings = settings();
+        return new OpenAIClientBuilder().endpoint(settings.getEndpoint()).credential(new AzureKeyCredential(settings.getKey())).buildAsyncClient();
+    }
+
+    private AzureOpenAISettings settings() throws ConfigurationException {
+        Map<String, String> map = new HashMap<>();
+        map.put(AzureOpenAISettings.getKeySuffix(), azOaiKey);
+        map.put(AzureOpenAISettings.getAzureOpenAiEndpointSuffix(), azOaiEndpoint);
+        map.put(AzureOpenAISettings.getAzureOpenAiDeploymentNameSuffix(), azOaiDeployment);
+        AzureOpenAISettings settings = new AzureOpenAISettings(map);
+        return settings;
+    }
+
+    private Kernel getKernel() {
+        return kernel;
+    }
+}
